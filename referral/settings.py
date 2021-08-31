d3f1e26ac51ccaac73ddb23e80e68236296d30b0
krishnamathuria <kmathuria13@gmail.com>
2021-09-01 01:21:59 +0530

    heroku compatible

--- referral/settings.py
+++ referral/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import os
+import django_heroku
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(p^(cgz_+5+#l_coln@8w+4_x3+z_l%6*e!az7xl4tfq1^fz0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -144,3 +145,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+django_heroku.settings(locals())
